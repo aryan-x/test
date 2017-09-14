db727d1d520e5dc3239b0a03b75c1b7039d7b4b7
Aryan <aryan.blackboy@gmail.com>
2017-09-15 04:22:53 +0430

    do

--- game.js
+++ game.js
@@ -647,6 +647,23 @@ AlienBullet = function () {
 };
 AlienBullet.prototype = new Bullet();
 
+this.breakIntoFragment = function(){
+	    if (this.scale > 0.5) {
+      // break into fragments
+      for (var i = 0; i < 3; i++) {
+        var roid = $.extend(true, {}, this);
+        roid.vel.x = Math.random() * 6 - 3;
+        roid.vel.y = Math.random() * 6 - 3;
+        if (Math.random() > 0.5) {
+          roid.points.reverse();
+        }
+        roid.vel.rot = Math.random() * 2 - 1;
+        roid.move(roid.scale * 3); // give them a little push
+        Game.sprites.push(roid);
+      }
+    }
+}
+
 Asteroid = function () {
   this.init("asteroid",
             [-10,   0,
@@ -672,20 +689,8 @@ Asteroid = function () {
     SFX.explosion();
     if (other.name == "bullet") Game.score += 120 / this.scale;
     this.scale /= 3;
-    if (this.scale > 0.5) {
-      // break into fragments
-      for (var i = 0; i < 3; i++) {
-        var roid = $.extend(true, {}, this);
-        roid.vel.x = Math.random() * 6 - 3;
-        roid.vel.y = Math.random() * 6 - 3;
-        if (Math.random() > 0.5) {
-          roid.points.reverse();
-        }
-        roid.vel.rot = Math.random() * 2 - 1;
-        roid.move(roid.scale * 3); // give them a little push
-        Game.sprites.push(roid);
-      }
-    }
+    this.breakIntoFragment();
+
     Game.explosionAt(other.x, other.y);
     this.die();
   };
